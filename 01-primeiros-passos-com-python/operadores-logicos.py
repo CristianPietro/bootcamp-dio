45841e135ef6e9c9080458ff1b50237964a2a2d6
CristianPietro <crpietro26@gmail.com>
2023-08-31 20:41:40 -0300

    aula de operadores lógicos

---
+++ 01-primeiros-passos-com-python/operadores-logicos.py
@@ -0,0 +1,23 @@
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+print(True and True)
+print(True and False)
+print(True or True)
+print(True or False)
+print(False or False)
+
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+
+print(exp)
+
+exp_2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+print(exp_2)
+
+saldo_sufiente = (saldo >= saque and saque <= limite)
+conta_especial_com_saldo_suficiente = (conta_especial and saldo >= saque)
+
+exp_3 = saldo_sufiente or conta_especial_com_saldo_suficiente
+print(exp_3)
