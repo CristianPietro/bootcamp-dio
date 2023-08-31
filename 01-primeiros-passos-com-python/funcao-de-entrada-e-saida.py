3efd5c4cd74e1543989f323639dddb1e248f52d5
CristianPietro <crpietro26@gmail.com>
2023-08-31 09:50:48 -0300

    aula sobre função de entrada e saída em python

---
+++ 01-primeiros-passos-com-python/funcao-de-entrada-e-saida.py
@@ -0,0 +1,8 @@
+nome = input("Informe sua nome: ")
+idade = input("Informe sua idade: ")
+
+
+print(nome, idade,)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#" ,end="...\n")
+print(nome, idade, sep="$")
