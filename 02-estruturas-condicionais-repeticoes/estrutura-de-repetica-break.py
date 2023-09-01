11ece3938f5acae14561d3840dccec4861ba1f9c
CristianPietro <crpietro26@gmail.com>
2023-09-01 20:45:51 -0300

    break

---
+++ 02-estruturas-condicionais-repeticoes/estrutura-de-repetica-break.py
@@ -0,0 +1,27 @@
+
+#BREAK com WHILE
+
+# while True:
+
+#     numero = int(input("Informe seu número: "))
+
+#     if numero == 10:
+#         break
+#     print(numero)
+# print("Você acertou o número, Parábens!")
+
+#BREAK com FOR
+
+# for numero in range(100):
+
+#     if numero == 10:
+#         break
+#     print(numero)
+# print("ok, você acertou!")
+
+
+for numero in range(100):
+
+    if numero == 12:
+        continue
+    print(numero, end= " ")
